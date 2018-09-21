bc32c5f8d98094c5b10e6f552da6e75d14b947c2
farandula <farandula@versoblanco.com>
2018-09-21 18:29:28 +0200

    Añadido archivo test

---
+++ Test_silabeador.py
@@ -0,0 +1,2 @@
+test_palabras=[cigüeña, construir, ansiedad, institución, día, cara, carpa, guerrero, pingüino, guirigai, caray, yerro, hierro, yermo, bíceps, psicólogo, aceptar, subrogar, subrepticio]
+test_correcto=[]
