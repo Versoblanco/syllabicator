2411681f28524276b9aef33dbafd991b3b50d5ca
farandula <farandula@versoblanco.com>
2019-05-05 15:46:55 +0200

    Modificada descripción. Refactor: empaquetado de variables de alfabeto y estructura silábica en funciones.

--- Silabeador.py
+++ Silabeador.py
@@ -1,15 +1,8 @@
 #coding=UTF-8
 
-# La funcionalidad está creada para la división silábica normativa de unidades discretas del español (conjuntos silábico, palabras y pseudopalabras). No es aplicable a la transcripción fonética ni el análisis métrico del verso.
+# La funcionalidad está creada para la división silábica normativa de unidades discretas del español (palabras y pseudopalabras). No es aplicable a otra lenguas, a la transcripción fonética ni al análisis métrico del verso, aunque podría adaptarse para los dos últimos casos con relativa facilidad.
 
-# Se aplica, adaptado a ortografía del español, la división silábica expuesta en Quilis, Tratado de fonética y fonología, p. 368 y ss.
-
-# La consonante no puede ser núcleo ni constituir sílaba por sí misma.
-
-# Las semivocales 'y, w' y la letra hache son tratadas como consonantes, por lo que algunas palabras, especialmente los extranjerismos, diferirán de la segmentación más adecuada, otros casos se acercan a lo que un hispanohablante diría si desconoce la fonética del idioma - 'w-his-key', 'billy', 's-pain', 've-ge-ta-ble', 'a-mat-xu'
-
-# Algunos prefijos ('sub', 'psi' o 'pso') producen errores 'su-bín-dice*', 'pa-rap-si-có-lo-go*', 'p-so-ria-sis*', estos elementos no siguen la regla general al percibirse como una unidad independiente cuya identidad de sentido se prioriza sobre el uso fonético, si bien existen casos límite donde el prefijo está prácticamente lexicalizado 'su-bra-yar / sub-ra-yar'. Esto no ocurre con otras palabras o pseudopalabras donde por no percibirse esa unidad el hispanohablante siempre agrupa la consonante con la vocal que le sigue: 'rap-so-da', 'pep-si', 'su-bli-me'
-
+# Se aplican las reglas de división silábica expuestas en Quilis, Tratado de fonética y fonología, p. 368 y ss.
 #   V=vocal C = consonante.
 
 #   1. VCV =      V-CV 'a-mo'
@@ -24,47 +17,59 @@
 #                 b) VV 'au-ra' (cerradas, abierta+cerrada átona, cerrada átona + abierta)
 
 
+# La consonante se define por no poder ser núcleo silábico ni constituir sílaba por sí misma. Las semivocales 'y, w' y la letra hache son tratadas siempre como consonantes, por lo que algunas palabras, especialmente los extranjerismos, reciben una segmentación incorrecta 'w-his-key', 'billy', mientras otros casos se acercan a la división hecha por un hispanohablante que desconociera la fonética del idioma original 's-pain', 've-ge-ta-ble', 'a-mat-xu'
 
-# ** Estas funciones no están en uso, en pruebas. Comienza en línea 46**
+# Algunos prefijos ('sub', 'psi' o 'pso') producen errores 'su-bín-dice*', 'pa-rap-si-có-lo-go*', 'p-so-ria-sis*', estos elementos no siguen la regla general de agrupamiento al percibirse como unidad con significado independiente cuya identidad de sentido se prioriza sobre el uso fonético, si bien existen casos límite donde el prefijo está prácticamente lexicalizado 'su-bra-yar / sub-ra-yar'. Esto no ocurre con otras palabras o pseudopalabras donde, por no apreciarse unidad semántica, el hispanohablante se rige por la norma y agrupa la consonante con la vocal que le sigue: 'rap-so-da', 'pep-si', 'su-bli-me'.
+
+def definir_consonantes():
+    consonantes = map(lambda word: unicode(word, 'utf-8'),
+                      ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'])
+    return consonantes
+
+def definir_vocales():
+    vocales = map(lambda word: unicode(word, 'utf-8'), ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ü'])
+    return vocales
+
+def definir_inseparables():
+    inseparables = map(lambda word: unicode(word, 'utf-8'), ['ch', 'll', 'rr', 'pr', 'pl', 'br', 'bl', 'fr', 'fl', 'gr', 'gl', 'kr', 'cr', 'kl', 'cl', 'dr', 'tr'])
+    return inseparables
 
 def definir_alfabeto():
-    consonantes = map(lambda word: unicode(word, 'utf-8'),
-                      ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'q', 'r', 's', 't', 'v', 'w',
-                       'x', 'y', 'z'])
-    vocales = map(lambda word: unicode(word, 'utf-8'), ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ü'])
-    alfabeto = consonantes+ vocales
+    alfabeto = definir_consonantes() + definir_vocales()
     return alfabeto
 
 def validar_letra(caracter):
     alfabeto = definir_alfabeto()
     if caracter in alfabeto:
         return caracter
-    else:
-        return ''
-
-# ** Comienza aquí **
+    return ''
 
 def es_vocal(letra):
-    vocales =map(lambda word: unicode(word, 'utf-8'), ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ü'])
+    vocales = definir_vocales()
     return letra in vocales
 
-
 def es_consonante(letra):
-    consonantes =  map(lambda word: unicode(word, 'utf-8'), ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'])
+    consonantes =  definir_consonantes()
     return letra in consonantes
 
-
 def es_inseparable(letra1, letra2):
-    inseparables = map(lambda word: unicode(word, 'utf-8'),
-                       ['ch', 'll', 'rr', 'pr', 'pl', 'br', 'bl', 'fr', 'fl', 'gr', 'gl', 'kr', 'cr', 'kl', 'cl', 'dr',
-                        'tr'])
+    inseparables = definir_inseparables()
     if not letra1 or not letra2:
         return False
     return letra1+letra2 in inseparables
 
-def es_diptongo(letra1, letra2):
+def VV(letra_anterior, letra):
+    return es_vocal(letra) and es_vocal(letra_anterior)
+
+def es_diptongo(letra_anterior, letra):
     vcerradas = map(lambda word: unicode(word, 'utf-8'), ['i', 'u', 'ü'])
-    return letra1 in vcerradas or letra2 in vcerradas
+    return letra_anterior in vcerradas or letra in vcerradas
+
+def es_hiato(letra_anterior, letra):
+    return not es_diptongo(letra_anterior, letra)
+
+def CV_(letra_anterior, letra):
+    return es_vocal(letra) and es_consonante(letra_anterior)
 
 def _CCV(letra, letra_siguiente, letra_consiguiente):
     return es_consonante(letra) and es_inseparable(letra, letra_siguiente) and es_vocal(letra_consiguiente)   # V-(C)CV, C-(C)CV
@@ -87,15 +92,16 @@ def buscar_letra_consiguiente(palabra, index):
     else:
         return palabra[index+2]
 
+def extraer_silaba(silabeo, silaba):
+    silabeo.append(silaba)
+
 def pedir_palabra():
     while True:
         palabra = raw_input('Escriba la palabra (exit para salir): ').decode('utf-8').strip()
         if palabra == 'exit':
-            print 'Adiós'
             exit()
         if len(palabra) > 0:
             return palabra
-
 
 def dar_formato(palabra):
     palabra = palabra.lower()
@@ -117,35 +123,43 @@ def silabear(palabra):
             silaba=letra
             continue
 
-        elif es_vocal(letra) and es_consonante(letra_anterior):           # Estructura CV, la vocal se agrupa siempre con la consonante
+        elif CV_(letra_anterior, letra):           # La vocal se agrupa siempre con la consonante anterior
             silaba=silaba+letra
+            continue
 
-        elif es_vocal(letra) and es_vocal(letra_anterior):                     # Estructura VV
+        elif VV(letra_anterior, letra):
 
-            if es_diptongo(letra_anterior, letra):                                       # Diptongo VV
+            if es_diptongo(letra_anterior, letra):
                 silaba=silaba+letra
-            else:
-                silabeo.append(silaba)
+                continue
+            if es_hiato(letra_anterior, letra):
+                extraer_silaba(silabeo, silaba)
                 silaba = letra
+                continue
 
-        elif es_consonante(letra):                                        #Estructuras C(C)V, C(C)C, V(C)V, V(C)C
+        #Estructuras C(C)V, C(C)C, V(C)V, V(C)C
 
-            if _CCV(letra, letra_siguiente, letra_consiguiente) or _CV(letra_anterior, letra, letra_siguiente):        
-                silabeo.append(silaba)
-                silaba = letra
-            else:
-                silaba=silaba+letra
+        elif _CCV(letra, letra_siguiente, letra_consiguiente) or _CV(letra_anterior, letra, letra_siguiente):
+            extraer_silaba(silabeo, silaba)
+            silaba = letra
+            continue
+
+        # Resto de consonantes
+        elif es_consonante(letra):
+            silaba=silaba+letra
+            continue
 
         else:
             print 'Error'
             break
-    silabeo.append(silaba)
+    extraer_silaba(silabeo, silaba)
     return silabeo
 
 def silabeador():
     while True:
         palabra = pedir_palabra()
         palabra = dar_formato(palabra)
+        print 'Silabeo de ' + palabra
         silabeo = silabear(palabra)
         silabeo = '-'.join(silabeo)
         print silabeo
