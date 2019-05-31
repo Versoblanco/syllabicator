47a419e89f7c016fa71030c8bc5c606db15a2a55
farandula <farandula@versoblanco.com>
2019-05-31 21:01:41 +0200

    Refactor. Ampliación funcionalidad a semivocales. Mejora de test. Documentación.

--- Silabeador_recursivo.py
+++ Silabeador_recursivo.py
@@ -1,24 +1,5 @@
 # encoding=Utf-8
-# La funcionalidad está creada para la división silábica normativa de unidades discretas del español (palabras y pseudopalabras). No es aplicable a otras lenguas, a la transcripción fonética ni al análisis métrico del verso, pero se ha intentado crear una estructura flexible, que permita la reutilización y adaptación del código a otras lenguas y reglas.
 
-# Se aplican las reglas de división silábica expuestas en Quilis, Tratado de fonética y fonología, p. 368 y ss.
-#   V=vocal C = consonante.
-
-#   1. VCV =      V-CV 'a-mo'
-
-#   2. VCCV =     a) VC-CV 'ar-mo'
-#                 b) V-CCV 'a-bro' [(b, c, f, g, k, p) + (l, r), dr, tr, ch, ll, rr]
-
-#   3. VCCC(C)V = a)VCC (ns, bs) - (C)CV 'ins-tante', 'obs- truir'
-#                 b)VC-CCV 'in-tra' (grupo inseparable)
-
-#   4. VV =       a) V-V 've-a', 'le-í-a' (abiertas, abierta + cerrada tónica, cerrada tónica + abierta)
-#                 b) VV 'au-ra' (cerradas, abierta+cerrada átona, cerrada átona + abierta)
-
-
-# La consonante se define por no poder ser núcleo silábico ni constituir sílaba por sí misma. Las semivocales 'y, w' y la letra hache son tratadas siempre como consonantes, por lo que algunas palabras, especialmente los extranjerismos, reciben una segmentación incorrecta , 'billy', mientras otros casos se acercan a la división hecha por un hispanohablante que desconociera la fonética del idioma original: 've-ge-ta-ble', 'a-mat-xu'
-
-# Algunos prefijos ('sub', 'psi' o 'pso') producen errores 'su-bín-dice*' por 'sub-ín-di-ce', 'pa-rap-si-có-lo-go*' por 'pa-ra-psi-có-lo-go', etc., estos elementos no siguen la regla general de agrupamiento al percibirse como unidad con significado independiente cuya identidad de sentido se prioriza sobre el uso fonético, si bien existen casos límite donde el prefijo está prácticamente lexicalizado 'su-bra-yar / sub-ra-yar'. Esto no ocurre con otras palabras o pseudopalabras donde, por no apreciarse unidad semántica, el hispanohablante se rige por la norma y agrupa la consonante con la vocal que le sigue: 'rap-so-da', 'pep-si', 'su-bli-me'.
 
 def _get_encoding():
     encoding = 'utf-8'
@@ -34,45 +15,59 @@ def _decode_str(text):
 def _spa():
     consonants = map(lambda word: unicode(word, 'utf-8'), ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'ñ', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'])
     vowels = map(lambda word: unicode(word, 'utf-8'), ['a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú', 'ü'])
+    semivowels = map(lambda word: unicode(word, 'utf-8'), ['y'])
     indivisibleConsonants = map(lambda word: unicode(word, 'utf-8'), ['ch', 'll', 'rr', 'pr', 'pl', 'br', 'bl', 'fr', 'fl', 'gr', 'gl', 'kr', 'cr', 'kl', 'cl', 'dr', 'tr'])
     closedVowels = map(lambda text: unicode(text, 'utf-8'), ['i', 'u', 'ü'])
-    alphabet = [consonants, vowels, indivisibleConsonants, closedVowels]
+    alphabet = [consonants, vowels, semivowels,  indivisibleConsonants, closedVowels]
     return alphabet
 
 
-def _getConsonants(language):
+def _consonants(language):
     consonants = language[0]
     return consonants
 
 
-def _getVowels(language):
+def _vowels(language):
     vowels = language[1]
     return vowels
 
 
-def _getIndivisibleConsonants(language):
-    indivisibleConsonants = language[2]
+def _semivowels(language):
+    semivowels = language[2]
+    return semivowels
+
+
+def _indivisibleConsonants(language):
+    indivisibleConsonants = language[3]
     return indivisibleConsonants
 
 
-def _getClosedVowels(language):
-    closedVowels = language[3]
+def _closedVowels(language):
+    closedVowels = language[4]
     return closedVowels
 
 
 def _isVowel(letter):
-    vowels = _getVowels(_spa())
-    return letter in vowels
+    return letter in _vowels(_spa())
+
+
+def _isSemivowel(letter):
+    return letter in _semivowels(_spa())
+
+
+def _isVowelORSemivowel(letter):
+    return _isVowel(letter) or _isSemivowel(letter)
 
 
 def _isConsonant(letter):
-    consonants = _getConsonants(_spa())
-    return letter in consonants
+    return letter in _consonants(_spa())
 
 
 def _isIndivisible(letter):
-    consonants = _getIndivisibleConsonants(_spa())
-    return letter in consonants
+    return letter in _indivisibleConsonants(_spa())
+
+# V= vowel C = consonant
+
 
 def _VV(text, i):
     if len(text[i:]) >= 2:
@@ -80,9 +75,8 @@ def _VV(text, i):
 
 
 def _Diphthong(text, i):
-    closedVowels = _getClosedVowels(_spa())
     if len(text) >= 2 and _VV(text, i):
-        return text[i] in closedVowels or text[i+1] in closedVowels
+        return text[i] in _closedVowels(_spa()) or text[i+1] in _closedVowels(_spa())
 
 
 def _Hiatus(text, i):
@@ -97,32 +91,32 @@ def _CV(text):
 
 def _V_CV(text, i):
     if len(text[i:]) >= 3:
-        return _isVowel(text[i]) and _isConsonant(text[i+1]) and _isVowel(text[i+2])
+        return _isVowelORSemivowel(text[i]) and _isConsonant(text[i+1]) and _isVowelORSemivowel(text[i+2])
 
 
 def _V_CCV(text, i):
     if len(text[i:]) >= 4:
-        return _isVowel(text[i]) and _isIndivisible(text[i+1:i+3]) and _isVowel(text[i+3])
+        return _isVowelORSemivowel(text[i]) and _isIndivisible(text[i+1:i+3]) and _isVowelORSemivowel(text[i+3])
 
 
 def _VC_CV(text, i):
     if len(text[i:]) >= 4:
-        return _isVowel(text[i]) and _isConsonant(text[i+1]) and _isConsonant(text[i+2]) and _isVowel(text[i+3]) and not _isIndivisible(text[i+1:i+3])
+        return _isVowelORSemivowel(text[i]) and _isConsonant(text[i+1]) and _isConsonant(text[i+2]) and _isVowelORSemivowel(text[i+3]) and not _isIndivisible(text[i+1:i+3])
 
 
 def _VC_CCV(text, i):
     if len(text[i:]) >= 5:
-        return _isVowel(text[i]) and _isConsonant(text[i+1]) and _isIndivisible(text[i+2:i+4]) and _isVowel(text[i+4])
+        return _isVowelORSemivowel(text[i]) and _isConsonant(text[i+1]) and _isIndivisible(text[i+2:i+4]) and _isVowelORSemivowel(text[i+4])
 
 
 def _VCC_CV(text, i):
     if len(text[i:]) >= 5:
-        return _isVowel(text[i]) and _isConsonant(text[i+1]) and _isConsonant(text[i+2]) and _isConsonant(text[i+3]) and _isVowel(text[i+4]) and not _isIndivisible(text[i+2:i+4])
+        return _isVowelORSemivowel(text[i]) and _isConsonant(text[i+1]) and _isConsonant(text[i+2]) and _isConsonant(text[i+3]) and _isVowelORSemivowel(text[i+4]) and not _isIndivisible(text[i+2:i+4])
 
 
 def _VCC_CCV(text, i):
     if len(text[i:]) >= 6:
-        return _isVowel(text[i]) and _isConsonant(text[i+1]) and _isConsonant(text[i+2]) and _isIndivisible(text[i+3:i+5]) and _isVowel(text[i+5])
+        return _isVowelORSemivowel(text[i]) and _isConsonant(text[i+1]) and _isConsonant(text[i+2]) and _isIndivisible(text[i+3:i+5]) and _isVowelORSemivowel(text[i+5])
 
 
 def _getNewText(text, i):
@@ -134,11 +128,24 @@ def _addLetter(syllable, letter):
     return syllable + letter
 
 
-def _findSyllable(text, syllable):
-    i = 0
-    if _isConsonant(text[i]) or _Diphthong(text, i):
-        syllable = _addLetter(syllable, text[i])
-        return _findSyllable(_getNewText(text, i+1), syllable)
+def _findVowelORSemivowel(text):
+    def nextLetter(text, i):
+        if len(text) > i+1:
+            return text[i+1]
+
+    for i, letter in enumerate(text):
+        vowelIndex = i
+        if _Hiatus(text, i):
+            break
+        if _isVowelORSemivowel(letter) and _isConsonant(nextLetter(text, i)):
+            break
+    print vowelIndex
+    return vowelIndex
+
+
+def _findSyllable(text):
+    i = _findVowelORSemivowel(text)
+    syllable = text[:i]
     if _V_CCV(text, i) or _V_CV(text, i) or _Hiatus(text, i):
         syllable = _addLetter(syllable, text[i])
         return syllable
@@ -149,13 +156,13 @@ def _findSyllable(text, syllable):
         syllable = _addLetter(syllable, text[i:i+3])
         return syllable
     else:
-        syllable = _addLetter(syllable, text[:])
+        syllable = _addLetter(syllable, text[i:])
         return syllable
 
 
 def _Syllabification(text, syllabification):
     if len(text) > 0:
-        syllable = _findSyllable(text, '')
+        syllable = _findSyllable(text)
         syllabification.append(syllable)
         newText = _getNewText(text, len(syllable))
         return _Syllabification(newText, syllabification)
@@ -170,15 +177,16 @@ def syllabicate(text):
     return syllabification
 
 
-def pedir_palabra():
+def _askText():
     while True:
-        palabra = raw_input('Escriba la palabra (exit para salir): ')
-        if palabra == 'exit':
+        text = raw_input('Escriba la palabra (exit para salir): ')
+        if text == 'exit':
             exit()
-        if len(palabra) > 0:
-            return palabra
+        if len(text) > 0:
+            return text
 
-def dar_formato(palabra):
-    palabra = palabra.strip().lower()
-    palabra = ''.join(palabra)
-    return palabra
+
+def _formatText(text):
+    text = text.strip().lower()
+    text = ''.join(text)
+    return text
