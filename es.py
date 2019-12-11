4537b4ed21f863f8aba8d1f8d6a7068211cac643
Farándula <farandula@versoblanco.com>
2019-12-11 21:15:10 +0100

    Added AGPL 3.0, copyright and license notices

--- es.py
+++ es.py
@@ -2,6 +2,20 @@
 # -*- coding: utf-8 -*-
 """Spanish alphabet and syllabification rules."""
 
+# Copyright 2019 Patricia Martín (aka Farándula)
+
+# This file is part of syllabicator.
+# Syllabicator is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+# Syllabicator is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Syllabicator. If not, see <https://www.gnu.org/licenses/>.
 
 def _alphabet():
     consonants = u'bcdfghjklmnñpqrstvwxyz'
--- syllabicator.py
+++ syllabicator.py
@@ -2,6 +2,21 @@
 # -*- coding: utf-8 -*-
 """Syllabicate word according to rules of given language."""
 
+# Copyright 2019 Patricia Martín (aka Farándula)
+
+# This file is part of syllabicator.
+# Syllabicator is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+# Syllabicator is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
+# See the GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with Syllabicator. If not, see <https://www.gnu.org/licenses/>.
+
 
 def _get_syllable(word, lang):
     coda = lang.find_coda(word)
